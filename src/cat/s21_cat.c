0c5655e4eca5f8ea2ee0d50cb1ab88b18c2c9647
D Cox <bonnyped@student.21-school.ru>
2022-11-15 13:30:33 +0600

    preFinal

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -2,18 +2,19 @@
 #include <stdlib.h>
 #include <getopt.h>
 
-struct count {int b, e, n, s, t, E, T;} ;
-
+struct count {int b, e, n, s, t, v, E, T;} ;
 void check_options(int argc, char **argv);
 void usage(int argc, char *name_of_program);
-void print_flag_n(char current, char next, int *numbering);
-
-// void print(struct count);
+void print_flag_n(char current, char next, int *numbering, int number_of_string_in_opened_file);
+void print_flag_E(char current, char next, int number_of_string_in_opened_file);
+void print_flag_b(char current, char next, int number_of_string_in_opened_file, int *numbering);
+void print_flag_s(char current, char next, int *counter_n);
+void print_flag_T(char current);
+void print_flag_v(char current, int no_another_flag);
 
 int main(int argc, char **argv) {
     usage(argc, argv[0]);
     check_options(argc, argv);
-    
     return 0;
 }
 
@@ -26,8 +27,8 @@ void usage(int argc, char *name_of_program){
 
 void check_options(int argc, char **argv) {
     struct count flags = {0};
-    char c, current_char, next_char;
-    int number_of_element_of_file = 1, numbering = 1;
+    char c, current_char, next_char, post_next_char;
+    int number_of_element_of_file = 1, numbering = 1, counter_n = 1, count_of_flags = 0;
     static struct option long_opts[] = {
         {"number-nonblank", 0, 0, 'b'},
         {"number", 0, 0, 'n'},
@@ -36,7 +37,8 @@ void check_options(int argc, char **argv) {
     };
 
     while(c != -1){     
-        if((c = getopt_long(argc, argv, "+benstET", long_opts, NULL)) != -1) {           
+        if((c = getopt_long(argc, argv, "+benstvET", long_opts, NULL)) != -1) {           
+        count_of_flags++;
         switch(c) {
             case 'b': 
                 flags.b = 'b';
@@ -66,57 +68,91 @@ void check_options(int argc, char **argv) {
                 flags.T = 'T';
                 number_of_element_of_file = 2; 
                 break;
+            case 'v': 
+                flags.v = 'v';
+                number_of_element_of_file = 2; 
+                break;
             }    
         } 
     }
     
     while(number_of_element_of_file < argc) {
     FILE *file_to_open;
-    if((file_to_open = fopen(argv[number_of_element_of_file], "r")) == NULL) {
-        printf("Нет такого файла или каталога\n");
+    if((file_to_open = fopen(argv[number_of_element_of_file], "r")) == NULL) { printf("Нет такого файла или каталога\n"); 
     } else {
         if ((next_char = fgetc(file_to_open)) != EOF) {
         int number_of_string_in_opened_file = 1;
         while((current_char = next_char)!= EOF) {
-            // printf("1%c1\n", current_char); 
             next_char = fgetc(file_to_open);
             if (number_of_element_of_file == 1){
             printf("%c", current_char);
-            current_char = next_char;
             } else {
-                if(flags.n == 'n') {
-                    if(number_of_string_in_opened_file == 1){
-                        printf("%6d  ", numbering);
-                        numbering++;
-                        number_of_string_in_opened_file++;
-                    }
-                    print_flag_n(current_char, next_char, &numbering); 
-                    current_char = next_char;
-            }
-        }
-        }
+                if(flags.e != 0) { flags.E = flags.v = count_of_flags = 2; }
+                if(flags.t != 0) { flags.T = flags.v = count_of_flags = 2; }
+                if(flags.n != 0) { print_flag_n(current_char, next_char, &numbering, number_of_string_in_opened_file); }
+                if(flags.E != 0) { print_flag_E(current_char, next_char, number_of_string_in_opened_file); }
+                if(flags.b != 0) { print_flag_b(current_char, next_char, number_of_string_in_opened_file, &numbering); }
+                if(flags.s != 0) { print_flag_s(current_char, next_char, &counter_n); }
+                if(flags.T != 0) { print_flag_T(current_char); }
+                if(flags.v != 0) { print_flag_v(current_char, count_of_flags); }
+                }
+                if (current_char == '\n') { number_of_string_in_opened_file++; }
         }
         fclose(file_to_open);
-    number_of_element_of_file++;
-    } 
-}
+        number_of_element_of_file++;
+        } 
+    }
+    }
 }
 
-void print_flag_n(char current, char next, int *numbering) {
+void print_flag_n(char current, char next, int *numbering, int number_of_string_in_opened_file) {
+    if(number_of_string_in_opened_file == 1){
+        printf("%6d  ", *numbering);
+        *numbering = *numbering + 1;
+    }
     if (current == '\n'){
         if (next != EOF) {
         printf("%c%6d  ", current, *numbering);
         *numbering = *numbering + 1;
-        } else {
-            printf("%c", current);
-        }
-        } else {
-            printf("%c", current);
-            }
+        } else { printf("%c", current); } 
+    } else { printf("%c", current); }
 }
 
+void print_flag_E(char current, char next, int number_of_string_in_opened_file) {
+    if(current == '\n' && number_of_string_in_opened_file == 1) { printf("$%c", current); }
+        if(next == '\n') { printf("%c$", current); }
+        else { printf("%c", current); }
+}
 
+void print_flag_b(char current, char next, int number_of_string_in_opened_file, int *numbering) {
+    if(current == '\n' && number_of_string_in_opened_file == 1 && next != '\n'){
+            printf("%c%6d  ", current, *numbering);
+            *numbering = *numbering + 1;
+            } else if(current != '\n' && number_of_string_in_opened_file == 1) {
+                printf("%6d  %c", *numbering, current);
+                *numbering = *numbering + 1;
+                } else if(current == '\n' && next == '\n') { printf("%c", current); 
+                } else if(current == '\n' && next != '\n' && next != EOF) {
+                    printf("%c%6d  ", current, *numbering);
+                    *numbering = *numbering + 1;
+                    } else { printf("%c", current); }
+}
 
-// void print_flag_b(struct count) {
-//     printf(" %d  %d  %d  %d  %d  %d  %d ", flags.b, flags.e, flags.n, flags.s, flags.t, flags.E, flags.T);
-// }
+void print_flag_s(char current, char next, int *counter_n) {
+    if (*counter_n < 3) { printf("%c", current); }
+    else;
+    if(current == '\n' && next == '\n') {*counter_n = *counter_n + 1; }
+    if(next != '\n') { *counter_n = 1; }
+}
+
+void print_flag_T(char current) {
+    if(current == 9) { printf("^I"); }
+    else printf("%c", current);
+}
+
+void print_flag_v(char current, int no_another_flag) {
+    if(current <= 31 && current >= 0 && current != 9 && current != 10) {
+        printf("%c%c", '^', (current += 64));
+    } else if(current == 127) { printf("%c%c", '^', (current - 64)); }
+    else if (no_another_flag == 1) printf("%c", current);
+}
